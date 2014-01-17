76dc280134923f48e95f82defc8ec5ad40d05361
Dan Fraser <dfraser@capybara.org>
2014-01-17 02:05:12 -0500

    turn some lights on in the morning

---
+++ morning_lights.pl
@@ -0,0 +1,13 @@
+if (time_now("7:10") and $Weekday) {
+	$dining_rm_ceiling->set(ON);
+	$kitchen_ceiling->set(ON);
+	$front_dr_ceiling->set(ON);
+	$living_rm_lamp->set(ON);
+	$hall_ceiling->set(ON);
+	$bedside_r->set(ON);
+}
+
+if (time_now("9:00") and $Weekday) {
+	$All_Lights->set(OFF);
+}
+
