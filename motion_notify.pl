37db7d7d5c5dd56a20f8af7173aea08cbf8f8299
Dan Fraser <dfraser@capybara.org>
2014-06-21 21:34:22 -0400

    added motion sensor control of the light by the front door.

--- motion_notify.pl
+++ motion_notify.pl
@@ -2,15 +2,27 @@ my $motion_timer = new Timer();
 
 if ($front_dr_motion->state_now() eq ON) {
 	print_log("motion detected");
-	if ($motion_timer->inactive()) {
-		print_log("sending motion notification");
-		$motion_timer->set(3600);
-		notify_pushover("Motion","Motion Detected at Front Door");
+	if ($Dark) {
+		print_log("..and it's dark");
+		$motion_timer->set(600);
+		if ($front_dr_ceiling->state() eq OFF) {
+			print_log("...and the light was off");
+	        	$front_dr_ceiling->set("50%","front_dr_motion");
+		} else {
+			print_log("...but the light was already on.");	
+		}
 	} else {
-		print_log("motion notification prevented by timer");
+		print_log("...but it's light.");	
 	}
 }
 
 if ($motion_timer->expired()) {
-	$front_dr_motion->set(OFF);
+	print_log("motion sensor timer expired");
+	if ($front_dr_ceiling->get_set_by() eq "front_dr_motion") {
+		print_log("light was still set by motion sensor, turning it off");
+		$front_dr_ceiling->set(OFF);
+	} else {
+		print_log("light was set by ".$front_dr_ceiling->get_set_by()." so leaving it on!");
+	}
 }
+
